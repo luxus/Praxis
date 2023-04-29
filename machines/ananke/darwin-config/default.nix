ae8b1985dc91b917c2184352cd170bc534ee5efe
Mikaela Allan <me@mikaela.ca>
2023-04-29 11:00:27 -0400

    use MacGPG2 provided gpg for git signing on ananke

--- machines/ananke/darwin-config/default.nix
+++ machines/ananke/darwin-config/default.nix
@@ -24,6 +24,12 @@
   home-manager = {
     sharedModules = [
       inputs.nixvim.homeManagerModules.nixvim
+
+      ({lib, ...}: {
+        # Redirect to locally installed MacGPG2 gpg2 toolchain
+        # - Use override priority 900 to override any existing mkDefaults at oerride priority 1000
+        programs.git.signing.gpgPath = lib.mkOverride 900 "/usr/local/MacGPG2/bin/gpg2";
+      })
     ];
 
     users.em = builtins.removeAttrs cell.homeConfigurations."em@ananke" ["bee"];
