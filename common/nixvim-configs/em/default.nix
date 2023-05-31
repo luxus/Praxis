bef8b77366acdd76db8b4e52420f928c16055b77
Mikaela Allan <me@mikaela.ca>
2023-05-31 15:39:22 -0400

    nixvim: enable language server

--- common/nixvim-configs/em/default.nix
+++ common/nixvim-configs/em/default.nix
@@ -15,6 +15,10 @@
 in {
   colorscheme = "base16-scheme";
 
+  extraConfigLua = ''
+    vim.g.exrc = true
+  '';
+
   extraPackages = [
     pkgs.git
   ];
@@ -33,5 +37,75 @@ in {
     bufferline.enable = true;
     neogit.enable = true;
     nix.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+        bashls.enable = true;
+        clangd.enable = true;
+        hls.enable = true;
+        rnix-lsp.enable = true;
+        yamlls.enable = true;
+      };
+    };
+    lsp-lines.enable = true;
+    luasnip.enable = true;
+    nvim-cmp = {
+      enable = true;
+
+      mappingPresets = ["insert"];
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+
+        # <C-b> (back) <C-f> (forward) for snippet placeholder navigation.
+        "<C-u>" = "cmp.mapping.scroll_docs(-4)"; # Up
+        "<C-d>" = "cmp.mapping.scroll_docs(4)"; # Down
+
+        "<CR>" = ''
+          cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }
+        '';
+
+        "<Tab>" = {
+          modes = ["i" "s"];
+          action = ''
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end
+          '';
+        };
+
+        "<S-Tab>" = {
+          modes = ["i" "s"];
+          action = ''
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end
+          '';
+        };
+      };
+
+      snippet.expand = "luasnip";
+
+      sources = [
+        {name = "nvim_lsp";}
+        {name = "luasnip";}
+        {name = "path";}
+        {name = "buffer";}
+      ];
+    };
   };
 }
